17ec224fce7148856e9f149f4b3a56a2f1874b32
carlos <carlos.ruiz@unisa.edu.mx>
2024-07-27 13:58:32 -0600

    agregue un archivo

---
+++ datosNumericosEnteros.py
@@ -0,0 +1,11 @@
+a = 10 
+b = 11
+c = 8
+print("valor de a: ",a)
+print("tipo de dato de a es: ",type(a))
+
+print("valor de b:",b)
+print("tipo de dato de b es:",type(b))
+
+print("valor de c: ",c)
+print("tipo de dato de c es: ",type(c))
