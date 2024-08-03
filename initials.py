d87fb779c739c7794fb51ae040ab4b41810ebc33
carlos <carlos.ruiz@unisa.edu.mx>
2024-08-03 15:13:37 -0600

    ejercicios 5

---
+++ initials.py
@@ -0,0 +1,8 @@
+
+print(" cccccccc               v       v")
+print("c                       v       v")
+print("c                       v       V")
+print("c                       v       V")
+print("c                       v       V")  
+print("c                       v       V")
+print(" cccccccc                  V")
---
+++ temperatura.py
@@ -0,0 +1,4 @@
+temperatura=int(input("cual es la temperatura actual"))
+
+resultado =(temperatura- 32) / 1.8
+print("temepratura en grados fabrenheit: ", resultado)
